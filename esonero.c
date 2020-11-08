47459016d0f4ebdf1d5a028db4dbad1ba09bff49
Fefefo <tardiolifederico@gmail.com>
2020-11-08 19:54:19 +0100

    do while

--- esonero.c
+++ esonero.c
@@ -14,25 +14,28 @@ int main() {
   strtok(inStr, "\n"); // Rimuovo lo "\n" a fine stringa che fgets() prende
 
   char b, key[strlen(inStr)], outStr[strlen(inStr)], verifica[strlen(inStr)];
-  
-  printf("Hai già la chiave di cifratura da utilizzare?\nSe sì digita 1, altrimenti digita 0\n");
-  scanf(" %c", &b); // Ho trovato online che basta mettere uno spazio prima di %c per non avere problemi con lo \n dell'input stream precedente
 
-  if (b == '1') { // Controllo se l'utente ha o non ha la chiave
-    while(strlen(key) < strlen(inStr)) { // Richiedo la chiave ogni volta che l'utente la inserisce più corta della stringa da cifrare
-      printf("Inserisci la chiave per cifrare di lunghezza uguale o maggiore rispetto alla stringa\n");
-      while(getchar() != '\n');
-      fgets(key, 128, stdin);
-      strtok(key, "\n");
+  do {
+    printf("Hai già la chiave di cifratura da utilizzare?\nSe sì digita 1, altrimenti digita 0\n");
+    scanf(" %c", &b); // Ho trovato online che basta mettere uno spazio prima di %c per non avere problemi con lo \n dell'input stream precedente
+    
+    if (b == '1') { // Controllo se l'utente ha o non ha la chiave
+      while(strlen(key) < strlen(inStr)) { // Richiedo la chiave ogni volta che l'utente la inserisce più corta della stringa da cifrare
+        printf("Inserisci la chiave per cifrare di lunghezza uguale o maggiore rispetto alla stringa\n");
+        while(getchar() != '\n');
+        fgets(key, 128, stdin);
+        strtok(key, "\n");
+      }
+      for(int i = 0; i < strlen(inStr); i++) // For per confrontare con lo XOR carattere per carattere per calcolarae la nuova stringa
+        outStr[i] = key[i] ^ inStr[i];
+    } else if (b == '0') {
+      for(int i = 0; i < strlen(inStr); i++) { // For per confrontare con lo XOR carattere per carattere subito dopo averlo generato per calcolarae la nuova stringa
+        key[i] = 32 + (rand() % 96);
+        outStr[i] = key[i] ^ inStr[i];
+      }
     }
-    for(int i = 0; i < strlen(inStr); i++) // For per confrontare con lo XOR carattere per carattere per calcolarae la nuova stringa
-      outStr[i] = key[i] ^ inStr[i];
-  } else {
-    for(int i = 0; i < strlen(inStr); i++) { // For per confrontare con lo XOR carattere per carattere subito dopo averlo generato per calcolarae la nuova stringa
-      key[i] = 32 + (rand() % 96);
-      outStr[i] = key[i] ^ inStr[i];
-    }
-  }
+  } while(b != '0' && b != '1');
+
 
   printf("Input: %s\n", inStr);
   printf("Output: %s'\n'", outStr);
